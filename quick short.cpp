485d797ac114ef177f8c9f1a95e87dfa5ac97d77
Mohammad Zamzuli Qoricho <45739315+ZamzuliRicho@users.noreply.github.com>
2019-03-20 23:11:54 +0700

    Add files via upload

---
+++ quick short.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void quicksort( int simpan[], int kiri, int kanan){
+    int i=kiri, j=kanan;
+    int tmp;
+    int pembagian=simpan[(kiri+kanan)/2];
+
+    while(i<=j){
+        while(simpan[i]<pembagian)
+            i++;
+        while(simpan[j]>pembagian)
+            j--;
+        if(i<=j){
+            tmp=simpan[i];
+            simpan[i]=simpan[j];
+            simpan[j]=tmp;
+            i++;
+            j--;
+        }
+    }
+
+    if(kiri<j)
+        quicksort(simpan,kiri,j);
+    if(i<kanan)
+        quicksort(simpan,i,kanan);
+}
+main(){
+    int simpan[100];
+    int i,n,lb,ub;
+    lb=0;
+    cout<<"\t\t\tPROGRAM QUICK SORT ASCENDING\n";
+    cout<<"\t\t\t_____________________________ \n\n";
+    cout<<"\t\t\tmasukkan banyak data yang di urut: ";
+	cin>>n;
+
+    ub=n;
+    cout<<"masukkan data-datanya: \n\n";
+    for(i=1; i<=n; i++){
+        cout<<"\tdata ke- "<<i<<" : "; cin>>simpan[i];
+    }
+
+    quicksort(simpan,lb,ub);
+    cout<<"\nHasil pengurutan data: ";
+    for(i=1; i<=n; i++)
+        cout<<" "<<simpan[i];
+    getch();
+}
